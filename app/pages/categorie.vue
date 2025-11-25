2afe7d8bfaf0bc72c8f78e929c962feed7fa595e
Neil <mickeykr7@gmail.com>
2025-11-25 15:56:38 +0100

    categorie page zonder knoppen 25/11

---
+++ app/pages/categorie.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  
+<HeaderComponent/>
+<br>    </br>
+<p class=" text-space-tekst text-center text-4xl underline decoration-space-blue decoration-2 rounded-2xl">Kies een categorie!</p>
+</template>
+
+<style scoped>
+
+</style>
