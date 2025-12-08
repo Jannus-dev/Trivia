7df55efdb6d3877f486b512ab9a24498ddd2d181
Jannus_Dev <jan.honing2@gmail.com>
2025-12-08 22:13:58 +0100

    Randommize antwoorden

--- app/pages/quizpagina.vue
+++ app/pages/quizpagina.vue
@@ -62,6 +62,18 @@ async function fetchRandomQuestion() {
       .select('*')
       .eq('question_id', question.value.id)
 
+  choices.value = shuffleArray(choiceData ?? [])
+
+// Hulpfunctie om een array te husselen
+  function shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j], array[i]]
+    }
+    return array
+  }
+
+
   choices.value = choiceData ?? []
   loading.value = false
 }
